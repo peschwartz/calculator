a2c346536e34f6bafd3c8c41144304492f49fa1f
Phoebe Schwartz <peschwartz@wisc.edu>
2023-09-20 17:21:42 -0500

    addFunction works

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -4,22 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void goToActivity(String s) {
+    public void goToActivity(int sol) {
         Intent intent = new Intent(this, solution.class);
-        intent.putExtra("message", s);
+        intent.putExtra("solution", String.valueOf(sol));
         startActivity(intent);
     }
     public void addFunction(View view){
-        EditText myTextField = (EditText) findViewById(R.id.firstNum);
+        EditText firstNumber = (EditText) findViewById(R.id.firstNum);
+        EditText secondNumber = (EditText) findViewById(R.id.secondNum);
 
-        goToActivity(myTextField.getText().toString());
-        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
-        //Log.i("INFO", "Button Pressed");
+        try {
+            int num1 = Integer.parseInt(firstNumber.getText().toString());
+            int num2 = Integer.parseInt(secondNumber.getText().toString());
+            System.out.println(num1);
+            System.out.println(num2);
+
+            goToActivity(num1 + num2);
+
+        } catch (NumberFormatException e) {
+            Toast.makeText(MainActivity.this, "Please enter a valid number.", Toast.LENGTH_LONG).show();
+            Log.i("ERROR", "Not a number");
+
+        }
     }
 
     @Override
--- app/src/main/java/com/cs407/calculator/solution.java
+++ app/src/main/java/com/cs407/calculator/solution.java
@@ -2,13 +2,20 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class solution extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.solution_activity);
+        textView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("solution");
+        textView.setText(str);
     }
 }
